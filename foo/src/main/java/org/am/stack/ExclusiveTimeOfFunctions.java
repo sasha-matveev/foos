73b281e6266837999d4416d72f56e3feba7e4af5
alexander matveev <dev.alexander.matveev@gmail.com>
2026-03-06 23:38:54 +0100

    6

---
+++ foo/src/main/java/org/am/stack/ExclusiveTimeOfFunctions.java
@@ -0,0 +1,62 @@
+package org.am.stack;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Stack;
+
+public class ExclusiveTimeOfFunctions {
+
+    public static void main(String[] args) {
+        ExclusiveTimeOfFunctions s = new ExclusiveTimeOfFunctions();
+        System.out.println(Arrays.toString(s.exclusiveTime(2, List.of("0:start:0", "1:start:2", "1:end:5", "0:end:6")))); // 3 4
+        System.out.println(Arrays.toString(s.exclusiveTime(1, List.of("0:start:0", "0:start:2", "0:end:5", "0:start:6", "0:end:6", "0:end:7")))); // 8
+        System.out.println(Arrays.toString(s.exclusiveTime(2, List.of("0:start:0", "0:start:2", "0:end:5", "1:start:6", "1:end:6", "0:end:7")))); // 7 1
+
+    }
+
+
+    public int[] exclusiveTime(int n, List<String> logs) {
+        int[] funcs = new int[n];
+        Stack<Log> stack = new Stack<>();
+        Log lastLog = null;
+        for (String str : logs) {
+            Log log = new Log(str);
+            if (stack.isEmpty()) {
+                stack.push(log);
+            } else {
+                switch (log.op) {
+                    case start -> {
+                        Log prev = stack.peek();
+                        funcs[prev.id] += log.timestamp - lastLog.timestamp;
+                        stack.push(log);
+                    }
+                    case end -> {
+                        stack.pop();
+                        funcs[log.id] += log.timestamp - lastLog.timestamp;
+                    }
+                }
+            }
+            lastLog = log;
+        }
+
+        return funcs;
+    }
+
+    private static class Log {
+        int id;
+        Op op;
+        int timestamp;
+
+        public Log(String log) {
+            String[] split = log.split(":");
+            id = Integer.parseInt(split[0]);
+            op = Op.valueOf(split[1]);
+            timestamp = Integer.parseInt(split[2]) + (op == Op.end ? 1 : 0);
+        }
+    }
+
+    enum Op {
+        start,
+        end,
+    }
+}
