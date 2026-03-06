93bc60a26f2bac702cdf885b035903d603733dcf
alexander matveev <dev.alexander.matveev@gmail.com>
2026-03-06 21:24:01 +0100

    x

---
+++ foo/src/main/java/org/am/LongestSubstringWithoutRepeatingCharacters.java
@@ -0,0 +1,41 @@
+package org.am;
+
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
+/**
+ * <a href="https://leetcode.com/problems/longest-substring-without-repeating-characters/description/">leetcode</a>
+ */
+public class LongestSubstringWithoutRepeatingCharacters {
+    public static void main(String[] args) {
+        System.out.println(new  LongestSubstringWithoutRepeatingCharacters().lengthOfLongestSubstring("abcabcbb"));
+        System.out.println(new  LongestSubstringWithoutRepeatingCharacters().lengthOfLongestSubstring("bbbbb"));
+        System.out.println(new  LongestSubstringWithoutRepeatingCharacters().lengthOfLongestSubstring("pwwkew"));
+        System.out.println(new  LongestSubstringWithoutRepeatingCharacters().lengthOfLongestSubstring("123456789"));
+    }
+
+    public int lengthOfLongestSubstring(String s) {
+        Set<Character>[] candidates = new HashSet[s.length()];
+        int lastFinished = 0;
+
+        for (int i = 0; i < s.length(); i++) {
+            char sym = s.charAt(i);
+
+            candidates[i] = new HashSet<>();
+
+            for (int j = lastFinished; j <= i; j++) {
+                if (!candidates[j].add(sym)) {
+                    lastFinished = j;
+                }
+            }
+        }
+
+        int longest = 0;
+        for (Set<Character> candidate : candidates) {
+            longest = Math.max(longest, candidate.size());
+        }
+
+        return longest;
+    }
+}
--- foo/src/main/java/org/am/mongo/IFoosCollection.java
+++
@@ -1,6 +0,0 @@
-package org.am.mongo;
-
-import org.springframework.data.mongodb.repository.ReactiveMongoRepository;
-
-public interface IFoosCollection extends ReactiveMongoRepository<FooDocument, String> {
-}
