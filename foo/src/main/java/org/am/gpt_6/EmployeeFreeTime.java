7983b90743b40ee4906b0227861495cf64a1816b
alexander matveev <dev.alexander.matveev@gmail.com>
2026-03-18 20:05:17 +0100

    day 6.2

---
+++ foo/src/main/java/org/am/gpt_6/EmployeeFreeTime.java
@@ -0,0 +1,57 @@
+package org.am.gpt_6;
+
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+import java.util.PriorityQueue;
+
+public class EmployeeFreeTime {
+    public List<Interval> employeeFreeTime(List<List<Interval>> schedule) {
+        PriorityQueue<WorkerInterval> queue = new PriorityQueue<>(Comparator.comparingInt(wi -> wi.interval.start));
+        Interval current = null;
+
+        for (int i = 0; i < schedule.size(); i++) {
+            queueNext(i, schedule, queue);
+        }
+        List<Interval> result = new ArrayList<>();
+        while (!queue.isEmpty()) {
+            WorkerInterval next = queue.poll();
+            if (current == null) {
+                current = next.interval;
+            } else if (current.endsEarlierThanStartOf(next.interval)) {
+                result.add(new Interval(
+                        current.end,
+                        next.interval.start
+                ));
+                current = next.interval;
+            } else {
+                current = current.mergeEnds(next.interval);
+            }
+            queueNext(next.workerId, schedule, queue);
+        }
+        return result;
+    }
+
+    private void queueNext(int workerId, List<List<Interval>> schedule, PriorityQueue<WorkerInterval> queue) {
+        List<Interval> intervals = schedule.get(workerId);
+        if (!intervals.isEmpty()) {
+            queue.add(new WorkerInterval(intervals.removeFirst(), workerId));
+        }
+    }
+
+    record Interval(int start, int end) {
+        private boolean endsEarlierThanStartOf(Interval next) {
+            return this.end < next.start;
+        }
+
+        private Interval mergeEnds(Interval next) {
+            return new Interval(this.start, Math.max(this.end, next.end));
+        }
+    }
+
+    record WorkerInterval(
+            Interval interval,
+            Integer workerId
+    ) {
+    }
+}
---
+++ foo/src/main/java/org/am/gpt_6/MeetingRoomsII.java
@@ -0,0 +1,22 @@
+package org.am.gpt_6;
+
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.PriorityQueue;
+
+public class MeetingRoomsII {
+    public int minMeetingRooms(int[][] intervals) {
+        Arrays.sort(intervals, Comparator.comparingInt(ar -> ar[0]));
+
+        PriorityQueue<Integer> running = new PriorityQueue<>();
+        int max = 0;
+        for (int[] interval : intervals) {
+            while (!running.isEmpty() && running.peek() <= interval[0]) {
+                running.poll();
+            }
+            running.add(interval[1]);
+            max = Math.max(max, running.size());
+        }
+        return max;
+    }
+}
